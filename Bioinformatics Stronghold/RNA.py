5c5dc76dce236b89adf07e28862637e9a6c009c2
AEnyioko <aenyioko090@gmail.com>
2024-08-24 21:25:30 -0500

    Removed a file

--- Bioinformatics Stronghold/RNA.py
+++ Bioinformatics Stronghold/RNA.py
@@ -1,4 +1,11 @@
-# transcribes DNA to RNA
+"""
+Transcribing DNA into RNA
+
+Given: A DNA string 't' having length at most 1000 nt.
+
+Return: The transcribed RNA string of 't'.
+"""
+
 def tr(input, output):
     """transcribes rna"""
     n = open(input, "r")
