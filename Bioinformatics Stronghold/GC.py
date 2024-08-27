294279b177e49d2daa711b118fc85130e51eff34
AEnyioko <aenyioko090@gmail.com>
2024-08-27 02:31:45 -0500

    Update GC.py

--- Bioinformatics Stronghold/GC.py
+++ Bioinformatics Stronghold/GC.py
@@ -8,31 +8,28 @@ Rosalind allows for a default error of 0.001 in all decimal answers unless other
 
 # created a dictionary to store sequence name and GC content value
 
-GC_dict = {}
-
-with open('RosalindData/rosalind_gc.txt', 'r') as GC:
-    for line in GC.readlines():      
-        if '>' in line:
-            key = line.strip()
-        else:
-            value = line.strip()
-
-            if key in GC_dict:
-                GC_dict[key].append(value)
-            else:
-                GC_dict[key] = [value]
-
-
 # calculates the GC content in each string
 
 def GC_Content(dict):
-    """Prints the GC content and the name of the greatest string"""
+    """Prints the name and value of the sequence with the highest GC content"""
+
+    GC_dict = {}
+    with open(dict, 'r') as GC:
+        for line in GC.readlines():      
+            if '>' in line:
+                key = line.strip()
+            else:
+                value = line.strip()
+
+                if key in GC_dict:
+                    GC_dict[key].append(value)
+                else:
+                    GC_dict[key] = [value]
 
     Content = {}
     maxi = ''
 
-
-    for k, v in dict.items():
+    for k, v in GC_dict.items():
         a = str(v)
         total = a.count('G') + a.count('C') + a.count('T') + a.count('A')
         GC_count = a.count('G') + a.count('C')
@@ -45,4 +42,4 @@ def GC_Content(dict):
 
     return maxi.replace('>', '') + '\n' + str(Content[maxi])
 
-print(GC_Content(GC_dict))
+print(GC_Content('RosalindData/rosalind_gc.txt'))
