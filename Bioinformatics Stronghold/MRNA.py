258afc5fb11376264778dd6290f9a949933729a7
AEnyioko <aenyioko090@gmail.com>
2024-08-28 13:08:51 -0500

    Finished MRNA problem

--- Bioinformatics Stronghold/MRNA.py
+++ Bioinformatics Stronghold/MRNA.py
@@ -7,14 +7,44 @@ Return: The total number of different RNA strings from which the protein could h
 (Don't neglect the importance of the stop codon in protein translation.)
 """
 
-from Bio.Seq import Seq, CodonTable
+# obtain protein codon table
+codons = {                                                     
+    'UUU': 'F',     'CUU': 'L',     'AUU': 'I',     'GUU': 'V',
+    'UUC': 'F',     'CUC': 'L',     'AUC': 'I',     'GUC': 'V',
+    'UUA': 'L',     'CUA': 'L',     'AUA': 'I',     'GUA': 'V',
+    'UUG': 'L',     'CUG': 'L',     'AUG': 'M',     'GUG': 'V',
+    'UCU': 'S',     'CCU': 'P',     'ACU': 'T',     'GCU': 'A',
+    'UCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
+    'UCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
+    'UCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
+    'UAU': 'Y',     'CAU': 'H',     'AAU': 'N',     'GAU': 'D',
+    'UAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
+    'UAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
+    'UAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
+    'UGU': 'C',     'CGU': 'R',     'AGU': 'S',     'GGU': 'G',
+    'UGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
+    'UGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
+    'UGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G' 
+}
 
-# obtain protein seq
 
-protein_seq = Seq('MA')
-standard_table = CodonTable.unambiguous_dna_by_name["Standard"]
+# obtain protein to analyze
+protein_seq  = ''
+with open ('RosalindData/rosalind_mrna (4).txt', 'r') as Prot:
+    for line in Prot:
+        protein_seq += line.strip()
 
-# identify sequences
+# checks how many variations a protein has
 
 for protein in protein_seq:
-    print(standard_table.columns)
+    count = 0
+    for k in codons:
+        if codons[k] == protein:
+            count += 1
+
+# multiplies each variation for each protein in the sequence
+    combo = combo * count
+
+# multiplied by 3 for the 3 variations of stop codons
+print(3 * combo % 1000000)
+
