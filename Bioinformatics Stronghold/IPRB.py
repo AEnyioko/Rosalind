2524cd316e3ef42807cb1588c4ac0983532590b8
AEnyioko <aenyioko090@gmail.com>
2024-08-29 14:43:01 -0500

    Finished IPRB

--- Bioinformatics Stronghold/IPRB.py
+++ Bioinformatics Stronghold/IPRB.py
@@ -11,5 +11,20 @@ an individual possessing a dominant allele (and thus displaying the dominant phe
 Assume that any two organisms can mate.
 """
 
-import random
+def mendel(k, m, n):
+    # gets the total
+    total = k + m + n
+
+    # calculates the chances of each combination
+    tworec = (n/total)*((n - 1)/(total - 1))
+    twohetero = (m/total)*((m - 1)/(total - 1))
+    hetrec = (n/total)*(m/(total - 1)) + (m/total)*(n/(total - 1))
+
+    # gets probability of recessive phenotype occuring
+    prob = tworec + twohetero*(1/4) + hetrec*(1/2)
+
+    #finds dominant by subtracting recessive from pop
+    return 1 - prob
+
+print(mendel(20, 20, 30))
 
