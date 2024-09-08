688d1b009708937cff5ade19502f4e1fe23df4e2
AEnyioko <aenyioko090@gmail.com>
2024-09-08 13:15:37 -0500

    Update CONS.py

--- Bioinformatics Stronghold/CONS.py
+++ Bioinformatics Stronghold/CONS.py
@@ -6,18 +6,14 @@ Given: A collection of at most 10 DNA strings of equal length (at most 1 kbp) in
 Return: A consensus string and profile matrix for the collection. (If several possible consensus strings exist, then you may return any one of them.)
 
 """
-
 import numpy as np
 
-with open('RosalindData/rosalind_cons.txt', 'r') as strings:
+with open('RosalindData/rosalind_cons.txt', 'r') as string:
+    strings = ''
+    for line in string.readlines(): # takes each string a seperates the kbps
+        if '>' not in line:
+             for n in line:
+                strings =  strings + n + ','
 
-    s = ""
-    for line in strings.readlines():      
-        if '>' in line:
-            key = line.strip()
-        else:
-            value = line.strip()
-            s += (value + "; ") 
-
-
-print(s)
+a = np.array(strings.split(',')).reshape(7,9) # converts it to array
+print(a)
