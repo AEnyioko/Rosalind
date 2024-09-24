0a451d25ebaa03be3a0b90dd8bf695152be3be4c
AEnyioko <aenyioko090@gmail.com>
2024-09-24 15:52:34 -0500

    Update IEV.py
    Added description of the problem

--- BioinfoStronghold/questions/IEV.py
+++ BioinfoStronghold/questions/IEV.py
@@ -1,3 +1,20 @@
+"""
+Calculating Expected Offspring
+
+Given: Six nonnegative integers, each of which does not exceed 20,000. 
+The integers correspond to the number of couples in a population possessing each genotype pairing for a given factor. 
+In order, the six given integers represent the number of couples having the following genotypes:
+1. AA-AA
+2. AA-Aa
+3. AA-aa
+4. Aa-Aa
+5. Aa-aa
+6. aa-aa
+
+Return: The expected number of offspring displaying the dominant phenotype in the next generation, 
+under the assumption that every couple has exactly two offspring.
+"""
+
 couples = {1: 1, 2: 1, 3: 1, 4: 0.75, 5: 0.5, 6: 0}
 offspring = [19992, 19579, 16055, 16655, 19422, 18434]
 
