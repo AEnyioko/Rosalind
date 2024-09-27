14a923a125abcc489faf994a486dc01d45390f43
AEnyioko <aenyioko090@gmail.com>
2024-09-26 19:36:35 -0500

    Updated LSCM.py

--- BioinfoStronghold/questions/LCSM.py
+++ BioinfoStronghold/questions/LCSM.py
@@ -5,6 +5,7 @@ Given: A collection of k(k ≤ 100) DNA strings of length at most 1 kbp each in 
 
 Return: A longest common substring of the collection. (If multiple solutions exist, you may return any single solution.)
 """
+from itertools import combinations
 
 def shared(file):
     # puts keys and strings in their lists
@@ -20,17 +21,20 @@ def shared(file):
     del strings[0]
     strings.append(string)
     
-    
-    i = 0
-    j = 999
-    substring = strings[0][i:j]
-    motif = ''
+    main_string = strings[0]
+    substring = sorted([main_string[x:y] for x, y in combinations(
+        range(len(main_string) + 1), r = 2)], key=len)
+
+    print(substring[0:10].reverse())
+
     for s in strings:
-        if substring not in s:
-            j -= 1
-            if j == 1:
-                j == len(strings[0])
-                i += 1
-            print(substring)
+        for ss in reversed(substring):
+            if ss not in strings:
+                continue
+            else:
+                if s == strings[-1]:
+                    print(ss)
+        
+    return None
 
 shared('RosalindData/rosalind_lcsm.txt')
