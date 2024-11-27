aa7245852fb7b53c5715f3911c6b9bfb4f47af42
AEnyioko <aenyioko090@gmail.com>
2024-11-26 18:17:10 -0600

    Update LEXF.py
    Solved the problem

--- BioinfoStronghold/questions/LEXF.py
+++ BioinfoStronghold/questions/LEXF.py
@@ -8,10 +8,21 @@ Return: All strings of length n that can be formed from the alphabet,
 ordered lexicographically (use the standard order of symbols in the English alphabet).
 """
 
-from itertools import permutations
+from itertools import product
 
-dna_nuc = "A C G T"
-k = 2
+def k_values(file):
+    #reads file
+    with open(file, 'r') as k:
+        symbols = k.readline().strip().split()
+        size = int(k.readline().strip())
 
-perm = permutations(dna_nuc)
-print(perm)
+    #prints values
+    k_mers = product(symbols, repeat=size)
+
+    return k_mers
+    
+for i in k_values('RosalindData/rosalind_lexf (1).txt'):
+    k_mer = ''
+    for n in i:
+        k_mer += n
+    print(k_mer)
