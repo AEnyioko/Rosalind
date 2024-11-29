788ae6ef7fff31f78a0c54726d2d95fd13da1314
AEnyioko <aenyioko090@gmail.com>
2024-11-29 00:58:02 -0600

    Update LGIS.py

--- BioinfoStronghold/questions/LGIS.py
+++ BioinfoStronghold/questions/LGIS.py
@@ -10,13 +10,32 @@ def longest():
 
 perm = [8, 2, 1, 6, 5, 7, 4, 3, 9]
 
-longest_perm = []
-longest_perm.append(perm[0])
+def longest_seq(permutation):
+    increasing = []
+    i_snapshot = []
+    decreasing = []
+    d_snapshot = []
 
-for i in range(len(perm)):
-    if perm[i] >= longest_perm[-1]:
-        continue
-    else:
-        longest_perm.append(perm[i])
+    for i in range(len(permutation)):
+        increasing.append(permutation[i])
+        for j in range(len(increasing)):
+            if permutation[i] > increasing[j]:
+                i_snapshot.append(increasing[0:-1])
+                increasing = increasing[:j]
+                increasing.append(permutation[i])
+                break
 
-print(longest_perm)
+    for i in range(len(permutation)):
+        decreasing.append(permutation[i])
+        for j in range(len(decreasing)):
+            if permutation[i] < decreasing[j]:
+                d_snapshot.append(decreasing[:-1])
+                decreasing = decreasing[:j]
+                decreasing.append(permutation[i])
+                break
+
+    print(max(d_snapshot, key=len), max(i_snapshot, key=len))
+
+    return None
+
+longest_seq(perm)
